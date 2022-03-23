e5cd0a7ffc61e038ef3c41ad9515ac5b46c46ff5
haoenting <91521504+haoenting@users.noreply.github.com>
2022-03-23 17:49:22 +0800

    Update E.py

--- week1/E.py
+++ week1/E.py
@@ -1,3 +1,27 @@
+Description
+Super idol 的笑容都沒你的甜
+八月正午的陽光都沒你耀眼
+熱愛 105°C 的你
+滴滴清纯的 n 個正整數，請判斷它們是不是質數。
+請設計一個 isP rime(x) 函式實作此題
+
+Input Format
+輸入的第一行為一個正整數 n (1 ≤ n ≤ 100)，代表輸入的數字數量。
+第二行有 n 個正整數 a1, a2, ..., an (1 ≤ ai ≤ 109
+, ∀i ∈ {1, 2, ..., n})
+
+Output Format
+對於每個 ai 輸出一行，若其為質數則輸出 True ，否則輸出 False (大小寫皆可)。
+
+Sample Input 
+3 
+3 4 5 
+
+Sample Output
+True
+False
+True
+
 def isPrime(x):
     bool = True
     if x == 1:
